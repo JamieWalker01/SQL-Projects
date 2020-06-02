18351208e014862f9eb1a4faa70ec6182e804020
Jamie Walker <63149925+JamieWalker01@users.noreply.github.com>
2020-06-02 14:10:26 +0100

    Update SQL Examples.sql

--- SQL Examples.sql
+++ SQL Examples.sql
@@ -1,4 +1,4 @@
--- Top 10 customers
+-- Top 10 Customers By Revenue 
 SELECT (c.FirstName ||' '|| c.LastName) AS Customer_name, 
 c.Country, 
 ROUND(SUM(i.Total),2) AS Total_Purchased
@@ -8,3 +8,19 @@ ON c.CustomerId = i.CustomerId
 GROUP BY c.FirstName, c.LastName
 ORDER BY 3 DESC
 LIMIT 10;
+
+-- Top 10 Countries By Revenue and Their Percentage of Total Income 
+SELECT  BillingCountry, 
+ROUND(SUM(Total),2) AS Total_Purchased, 
+ROUND(100.0 * ROUND(SUM(Total),2)/(SELECT SUM(Total) FROM invoices),2) AS Percent_of_Total
+FROM invoices 
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 10;
+
+-- Revenue By Month
+SELECT strftime("%Y-%m", InvoiceDate) AS Month, 
+SUM(Total) 
+FROM invoices
+GROUP BY 1
+ORDER BY 1;
