a8c6eb762bd242609eb052e9524423d5d16b1feb
Jamie Walker <63149925+JamieWalker01@users.noreply.github.com>
2020-06-09 13:49:06 +0100

    Update AdventureWorks_Queries.sql

--- AdventureWorks_Queries.sql
+++ AdventureWorks_Queries.sql
@@ -9,13 +9,14 @@ FROM
  ,SUM(s.SalesAmount) AS 'AVG_Monthly_Sales'
 FROM FactInternetSales s 
 GROUP BY YEAR(s.OrderDate), MONTH(s.OrderDate)) AS s
+-- Using an inner query to make an aggregate of an aggregate.
 GROUP BY Year, Month, AVG_Monthly_Sales
 ORDER BY Year, Month; 
 
 -- Yearly Running Total
 SELECT Year
 ,Month
-,SUM(Sales) OVER (PARTITION BY Year ORDER BY Month ROWS UNBOUNDED PRECEDING) as YTDSales
+,SUM(Sales) OVER (PARTITION BY Year ORDER BY Month ROWS UNBOUNDED PRECEDING) as YTD_Sales
 ,Sales AS Monthly_sales
 FROM
 (SELECT	
@@ -24,7 +25,7 @@ FROM
 ,year(s.OrderDate) AS 'Year'
 FROM FactInternetSales s
 GROUP BY MONTH(s.OrderDate), year(s.OrderDate)) AS S
-
+-- Again using an inner query to make an aggregate of an aggregate. 
 GROUP BY Year, Month, Sales
 ORDER BY Year, Month;
 			  
@@ -37,6 +38,7 @@ WITH MonthlySales_CTE (Year, Month, Sales) AS
 FROM DimDate d	
 JOIN FactInternetSales s ON d.DateKey = s.OrderDateKey
 GROUP BY d.CalendarYear, d.MonthNumberOfYear) 
+-- Using a common table expression to hold previous years sales.
 
 SELECT 
   d.CalendarYear AS Year
@@ -46,7 +48,7 @@ SELECT
 , (SUM(s.SalesAmount) - CTE.Sales)/ SUM(s.SalesAmount) AS Percent_Change
 FROM DimDate d
 JOIN FactInternetSales s ON d.DateKey = s.OrderDateKey
-JOIN MonthlySales_CTE CTE ON d.CalendarYear-1 = CTE.Year 
+JOIN MonthlySales_CTE CTE ON d.CalendarYear-1 = CTE.Year -- The CTE is joined on calenderyear-1 to assign it to the previous years sales.
 AND d.MonthNumberOfYear = CTE.Month
 GROUP BY d.CalendarYear, d.MonthNumberOfYear, CTE.Sales
 ORDER BY d.CalendarYear, d.MonthNumberOfYear;
