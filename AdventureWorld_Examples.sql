677abc70e412a476c186766b609814af5564a5ee
Jamie Walker <63149925+JamieWalker01@users.noreply.github.com>
2020-06-09 13:31:06 +0100

    Update AdventureWorld_Examples.sql

--- AdventureWorld_Examples.sql
+++ AdventureWorld_Examples.sql
@@ -18,13 +18,36 @@ SELECT Year
 ,SUM(Sales) OVER (PARTITION BY Year ORDER BY Month ROWS UNBOUNDED PRECEDING) as YTDSales
 ,Sales AS Monthly_sales
 FROM
-(SELECT	SUM(s.SalesAmount) AS 'Sales' 
-		,	MONTH(s.OrderDate) AS 'Month'
-		,	year(s.OrderDate) AS 'Year'
-	FROM	FactInternetSales s
-	GROUP BY
-			MONTH(s.OrderDate)
-		,	year(s.OrderDate)) AS S
+(SELECT	
+ SUM(s.SalesAmount) AS 'Sales' 
+,MONTH(s.OrderDate) AS 'Month'
+,year(s.OrderDate) AS 'Year'
+FROM FactInternetSales s
+GROUP BY MONTH(s.OrderDate), year(s.OrderDate)) AS S
+
 GROUP BY Year, Month, Sales
 ORDER BY Year, Month;
+			  
+-- Year on Year Monthly Sales Percent Change 
+WITH MonthlySales_CTE (Year, Month, Sales) AS
+(SELECT 
+  d.CalendarYear
+, d.MonthNumberOfYear
+, SUM(s.SalesAmount) 
+FROM DimDate d	
+JOIN FactInternetSales s ON d.DateKey = s.OrderDateKey
+GROUP BY d.CalendarYear, d.MonthNumberOfYear) 
+
+SELECT 
+  d.CalendarYear AS Year
+, d.MonthNumberOfYear AS Month
+, CTE.Sales AS Last_years_Sales
+, SUM(s.SalesAmount) AS Current_Sales
+, (SUM(s.SalesAmount) - CTE.Sales)/ SUM(s.SalesAmount) AS Percent_Change
+FROM DimDate d
+JOIN FactInternetSales s ON d.DateKey = s.OrderDateKey
+JOIN MonthlySales_CTE CTE ON d.CalendarYear-1 = CTE.Year 
+AND d.MonthNumberOfYear = CTE.Month
+GROUP BY d.CalendarYear, d.MonthNumberOfYear, CTE.Sales
+ORDER BY d.CalendarYear, d.MonthNumberOfYear;
                                   
