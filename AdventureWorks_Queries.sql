0d8f7dd42362b7e5274389975713e4c5a84f869f
Jamie Walker <63149925+JamieWalker01@users.noreply.github.com>
2020-06-09 13:37:46 +0100

    Update and rename AdventureWorld_Examples.sql to AdventureWorks_Queries.sql

--- AdventureWorks_Queries.sql
+++ AdventureWorks_Queries.sql
@@ -1,4 +1,4 @@
--- 3 month moving average of internet sales.
+-- 3 Month Moving Average of Internet Sales.
 SELECT Year
 , Month
 , AVG(AVG_Monthly_Sales) OVER(ORDER BY Year, Month ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) AS '3_Month_Moving_AVG'
